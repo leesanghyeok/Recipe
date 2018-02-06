d5b659211d339452e05d234d5fbd00d4c8c30ab8
MinHyeJun <m2j9702@naver.com>
2018-02-06 18:17:11 +0900

    MainActivity 메인 UI 생성

--- app/src/main/java/com/hey/blueberry/recipe/MainActivity.java
+++ app/src/main/java/com/hey/blueberry/recipe/MainActivity.java
@@ -1,14 +1,12 @@
 package com.hey.blueberry.recipe;
 
+import android.graphics.Color;
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.Toast;
-import android.content.Intent;
-import android.widget.Button;
-
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,10 +23,11 @@ public class MainActivity extends AppCompatActivity {
 
         //서치뷰 글씨 색상 설정
         SearchView searchView = (SearchView) findViewById(R.id.search_view);
-        SearchView.SearchAutoComplete searchAutoComplete = (SearchView.SearchAutoComplete)searchView.findViewById(android.support.v7.appcompat.R.id.search_src_text);
+        SearchView.SearchAutoComplete searchAutoComplete = (SearchView.SearchAutoComplete) searchView.findViewById(android.support.v7.appcompat.R.id.search_src_text);
         searchAutoComplete.setHintTextColor(Color.GRAY);
         searchAutoComplete.setTextColor(Color.WHITE);
-        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener(){
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
 
             @Override
             public boolean onQueryTextSubmit(String s) {
@@ -48,10 +47,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
-        switch (id)
-        {
-            case android.R.id.home:
-            {
+        switch (id) {
+            case android.R.id.home: {
                 // 즐겨찾기 버튼을 눌렀을 때 적절한 액션을 넣는다.
                 Toast.makeText(this, "즐겨찾기 버튼 누름", Toast.LENGTH_SHORT).show();
                 return true;
