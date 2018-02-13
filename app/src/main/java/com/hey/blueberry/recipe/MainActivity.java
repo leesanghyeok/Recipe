81ccef6c63c8f5cd8af517717418e61dcf6b7f13
Stark <stark.ssumu@gmail.com>
2018-02-13 19:24:01 +0900

    Revert "MainActivity 기본 UI 생성"
    This reverts commit 302b43586d823f03167dd9dcb93e3edf05d9ee57.

--- app/src/main/java/com/hey/blueberry/recipe/MainActivity.java
+++ app/src/main/java/com/hey/blueberry/recipe/MainActivity.java
@@ -1,12 +1,8 @@
 package com.hey.blueberry.recipe;
 
-import android.graphics.Color;
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.SearchView;
-import android.support.v7.widget.Toolbar;
-import android.view.MenuItem;
-import android.widget.Toast;
+import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,50 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //툴바의 뒤로가기를 활성화하여 즐겨찾기 버튼 아이콘으로 수정
-        Toolbar toolbar = (Toolbar) findViewById(R.id.mainToolbar);
-        setSupportActionBar(toolbar);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_star_border_white_24dp);
-
-        //서치뷰 글씨 색상 설정
-        SearchView searchView = (SearchView) findViewById(R.id.search_view);
-        SearchView.SearchAutoComplete searchAutoComplete = (SearchView.SearchAutoComplete)searchView.findViewById(android.support.v7.appcompat.R.id.search_src_text);
-        searchAutoComplete.setHintTextColor(Color.GRAY);
-        searchAutoComplete.setTextColor(Color.WHITE);
-
-        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener(){
-
-            @Override
-            public boolean onQueryTextSubmit(String s) {
-                //검색 시 동작할 코드
-                Toast.makeText(MainActivity.this, "검색", Toast.LENGTH_SHORT).show();
-                return false;
-            }
-
-            @Override
-            public boolean onQueryTextChange(String s) {
-                //입력 중 동작할 코드
-                return false;
-            }
-        });
+        TextView textView = (TextView) findViewById(R.id.text);
+        textView.setText("안녕 세계야.");
     }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-        switch (id)
-        {
-            case android.R.id.home:
-            {
-                // 즐겨찾기 버튼을 눌렀을 때 적절한 액션을 넣는다.
-                Toast.makeText(this, "즐겨찾기 버튼 누름", Toast.LENGTH_SHORT).show();
-                return true;
-            }
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-
-
 }
